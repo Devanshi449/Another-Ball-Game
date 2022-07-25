422ccc37cded8a278dbb49d62edbabc795c7f3b9
Devanshi Gupta <devanshigupta2002@gmail.com>
2022-07-26 02:35:37 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,56 @@
+import Ball from "./Ball.js";
+import Paddle from "./Paddle.js"
+
+const ball=new Ball(document.getElementById("ball"))
+const playerPaddle=new Paddle(document.getElementById("player-paddle"))
+const computerPaddle=new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem=document.getElementById("player-score")
+const computerScoreElem=document.getElementById("computer-score")
+
+
+let lasttime
+function update(time){ 
+    // console.log(time);
+    if(lasttime!=null)
+    {
+        const delta=time-lasttime
+        ball.update(delta,[playerPaddle.rect(),computerPaddle.rect()])
+        computerPaddle.update(delta,ball.y)
+        const hue=parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"))
+        // console.log(delta)
+        document.documentElement.style.setProperty("--hue",hue+delta*0.01)
+
+        if(isLose())
+        {
+            handleLose()
+        }
+    }
+
+    lasttime=time
+    window.requestAnimationFrame(update)
+}
+
+function isLose(){
+
+    const rect=ball.rect()
+    return rect.right>=window.innerWidth || rect.left<=0
+}
+
+function handleLose(){
+    const rect=ball.rect();
+    if (rect.right>=window.innerWidth) {
+        playerScoreElem.textContent=parseInt(playerScoreElem.textContent)+1
+        // console.log("playerScoreElem")
+    }
+    else{
+        computerScoreElem.textContent=parseInt(computerScoreElem.textContent)+1
+    }
+    ball.reset()
+    computerPaddle.reset()
+}
+
+document.addEventListener("mousemove", e=>{
+    playerPaddle.position=(e.y/window.innerHeight)*100
+})
+
+window.requestAnimationFrame(update)
